df9f3d4a0f1e3ef085409a9cc6d3911e1c368d90
@juanferatl <ferna283@hotmail.com>
2020-05-22 20:50:18 -0500

    Ejercicio Clase JAVA Ejecutar Rectangle desde Main.

--- src/main/java/com/edu/cedesistemas/oop/Main.java
+++ src/main/java/com/edu/cedesistemas/oop/Main.java
@@ -1,9 +1,16 @@
 package com.edu.cedesistemas.oop;
 
+import com.edu.cedesistemas.oop.model.geometry.Ellipse;
+import com.edu.cedesistemas.oop.model.geometry.Rectangle;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello Java OOP!");
-
+        Ellipse rectangle = new Ellipse(1, 2);
+        System.out.println("A: " + rectangle.getA());
+        System.out.println("B: " + rectangle.getB());
+        System.out.println("Area: " + rectangle.area());
+        System.out.println("Perimetro: " + rectangle.perimeter());
     }
 }
