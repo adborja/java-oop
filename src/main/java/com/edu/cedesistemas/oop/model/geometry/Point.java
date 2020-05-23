61987713c8cc56f12fa2e15252acfbdb01705ec2
sacgarci <sacgarci@bancolombia.com.co>
2020-05-23 00:22:55 -0500

    version inicial 2

---
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/Point.java
@@ -0,0 +1,33 @@
+package com.edu.cedesistemas.oop.model.geometry;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x,double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double distance(Point object1,Point object2) {
+        return x;
+    }
+
+    public Point of(double x,double y) {
+        Point point = new Point(x,y);
+        return point;
+    }
+
+    public Point random(int x) {
+        Point point = new Point(x,x);
+        return point;
+    }
+}
---
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/Rectangle.java
@@ -0,0 +1,53 @@
+package com.edu.cedesistemas.oop.model.geometry;
+
+public class Rectangle implements Shape{
+    private Point bl;
+    private Point tr;
+    private Point br;
+    private Point tl;
+    private double height;
+    private double width;
+
+    public Rectangle(Point object1,double height,double width) {
+
+        Point point = new Point(height,width);
+        this.bl = object1;
+        this.height = height;
+        this.width = width;
+    }
+
+    public Point getTopRight() {
+        return tr;
+    }
+
+    public Point getBottomleft() {
+        return bl;
+    }
+
+    public Point getBottomRight() {
+        return br;
+    }
+
+    public Point getTopLeft() {
+        return tl;
+    }
+
+    public double getwidth() {
+        return width;
+
+    }
+
+    public double getheight() {
+        return height;
+    }
+
+    @Override
+    public double area() {
+        return (Math.PI * height * width);
+    }
+
+    @Override
+    public double perimeter() {
+        return (2 * Math.PI * Math.sqrt((Math.pow(height, 2) + Math.pow(width, 2)) / 2));
+    }
+}
