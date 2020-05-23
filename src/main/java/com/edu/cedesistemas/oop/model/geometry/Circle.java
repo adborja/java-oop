80420f6f27a288476ae0f7b44b2d2c1e2b5f7a50
Jaider Andres Rojas Vanegas <JAANROJA@bancolombia.com.co>
2020-05-22 19:11:37 -0500

    Ejercicio Circulo viernes 22 de mayo

---
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/Circle.java
@@ -0,0 +1,17 @@
+package com.edu.cedesistemas.oop.model.geometry;
+
+public class Circle extends Ellipse {
+
+    public Circle(double radio) {
+        super(radio, radio);
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * getA();
+    }
+
+    public double getRadius() {
+        return getA();
+    }
+}
+
