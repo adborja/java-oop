a56b47de823b8a1d235505139b00907b7919217d
Leidyrpo <leidyrpo@gmail.com>
2020-05-29 17:59:30 -0500

    Taller Lección 2 – Arreglos

--- src/main/java/com/edu/cedesistemas/oop/arrays/ArrayUtils.java
+++ src/main/java/com/edu/cedesistemas/oop/arrays/ArrayUtils.java
@@ -8,19 +8,52 @@ public final class ArrayUtils {
 
     // Lesson 2 - - arrays
     public static void bubbleSort(Integer[] array) {
-        // Implemente codigo
+
+        boolean swapped = true;
+        Integer j = 0;
+        Integer tmp = 0;
+
+        while (swapped == true){
+            swapped = false;
+            j++;
+            for ( int i = 0; i < array.length - j; i++){
+                if (array[i] > array[i+1]) {
+                    tmp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = tmp;
+                    swapped = true;
+                }
+            }
+        }
     }
 
     // Lesson 2 -- ArrayList
     public static Integer sum(List<Integer> list) {
-        // Implemente codigo
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++){
+            sum = list.get(i)+sum;
+        }
+        return sum;
     }
 
     // Lesson 2 -- ArrayList
     public static List<Integer> getPrimeNumbers(int limit) {
-        // Implemente codigo
-        return null;
+        ArrayList<Integer> listaresultado = new ArrayList<Integer>();
+        int i = 0;
+        int num = 0;
+        int counter = 0;
+        for (i=1;i <= limit;i++){
+            counter = 0;
+            for (num=i;num >= 1;num--){
+                if (i%num == 0){
+                    counter += +1;
+                }
+            }
+            if(counter == 2){
+                listaresultado.add(i);
+            }
+        }
+        return listaresultado;
     }
 
     // Lesson 2 - arrays
