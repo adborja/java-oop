99e1d39d0c1283a98e2feea5d97cdd59707c99e1
yesolano <yenifers16@hotmail.com>
2020-05-29 18:03:23 -0500

    Actualización

---
+++ src/test/java/com/edu/cedesistemas/oop/arrays/ArrayUtilsTest.java
@@ -0,0 +1,41 @@
+package com.edu.cedesistemas.oop.arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayUtilsTest {
+    @Test
+    public void testMaxNumber() {
+        Integer[] numbers = {5, 8, 4, 9, 2};
+        int max = ArrayUtils.max(numbers);
+        assertThat(max, equalTo(9));
+    }
+
+    @Test
+    public void testSum() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        int sum = ArrayUtils.sum(numbers);
+        assertThat(sum, equalTo(15));
+    }
+
+    @Test
+    public void testBubbleSort() {
+        Integer[] array = {5, 9, 6, 1, 10, 4};
+        ArrayUtils.bubbleSort(array);
+        assertThat(array, arrayContaining(1, 4, 5, 6, 9, 10));
+    }
+
+    @Test
+    public void testPrimeNumbers() {
+        int limit = 20;
+        List<Integer> numbers = ArrayUtils.getPrimeNumbers(limit);
+        assertThat(numbers, contains(2, 3, 5, 7, 11, 13, 17, 19));
+    }
+}
