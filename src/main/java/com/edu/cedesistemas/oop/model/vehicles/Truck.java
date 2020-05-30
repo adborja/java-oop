82a063f02df80201c9af5ed7fa7dd1a0547312ad
Jaider Andres Rojas Vanegas <JAANROJA@bancolombia.com.co>
2020-05-29 19:46:21 -0500

    Ejercicio geometria miercoles 29 de mayo

---
+++ src/main/java/com/edu/cedesistemas/oop/model/vehicles/Truck.java
@@ -0,0 +1,9 @@
+package com.edu.cedesistemas.oop.model.vehicles;
+
+        import static java.lang.System.*;
+
+public class Truck extends FuelCar{
+    public Truck(double speed, String name, String type) {
+        super(speed, name, type);
+    }
+}
