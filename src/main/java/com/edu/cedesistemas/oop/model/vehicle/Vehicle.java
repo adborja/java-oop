ad1d9f4c97c0744c423c50f335a7448c10fa99e7
SrCarlangas <genered@gmail.com>
2020-06-03 18:13:00 -0500

    Taller 3

--- src/main/java/com/edu/cedesistemas/oop/model/vehicle/Vehicle.java
+++ src/main/java/com/edu/cedesistemas/oop/model/vehicle/Vehicle.java
@@ -7,14 +7,37 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface Vehicle {
-    double move(Point a, Point b);
-    List<Segment> getMovements();
+    Movement move(Point p1, Point p2);//Retorna el segmento entre 2 puntos
+    List<Movement> getMovements(); //lista de todos los movimientos que hizo el vehiculo
     void power();
     double getSpeed();
-    void setLastMaintenanceDate(LocalDateTime dateTime);
     LocalDateTime getLastMaintenanceDate();
+    void setLastMaintenanceDate(LocalDateTime date);
     double getTraveledKms();
     String getName();
     String getId();
     void setId(String id);
+
+    // Lesson 2 -- Inner classes
+    class Movement {
+        private final Segment segment;
+        private double time;
+
+        public Movement(Segment segment, double time) {
+            this.segment = segment;
+            this.time = time;
+        }
+
+        public double getTime() {
+            return time;
+        }
+
+        public void setTime(double time) {
+            this.time = time;
+        }
+
+        public Segment getSegment() {
+            return segment;
+        }
+    }
 }
