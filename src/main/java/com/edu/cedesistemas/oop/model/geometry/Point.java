42bb8a58482c937fcfbfbcbd3e932eb752bc3fe8
@juanferatl <ferna283@hotmail.com>
2020-06-05 18:47:18 -0500

    Ejerccio Race Car 1

--- src/main/java/com/edu/cedesistemas/oop/model/geometry/Point.java
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/Point.java
@@ -1,8 +1,12 @@
 package com.edu.cedesistemas.oop.model.geometry;
 
+import java.util.Objects;
+import java.util.Random;
+
 public class Point {
-    private double x;
-    private double y;
+    private final double x;
+    private final double y;
+
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
@@ -16,17 +20,36 @@ public class Point {
         return y;
     }
 
-    public static double distance(Point pt1, Point pt2) {
-        return 0;
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
     }
 
     public static Point of(double x, double y) {
-        Point pt = new Point(x, y);
-        return pt;
+        return new Point(x, y);
     }
 
-    public static Point random(int nro) {
-        Point pt = new Point(nro, nro);
-        return pt;
+    public static Point random(int bound) {
+        double x1 = new Random().nextDouble() * bound;
+        double y1 = new Random().nextDouble() * bound;
+        return of(x1, y1);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 &&
+                Double.compare(point.y, y) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
     }
 }
