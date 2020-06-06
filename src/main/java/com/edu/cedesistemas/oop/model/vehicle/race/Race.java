3fbd9f4bac9829fdda05e7f9d28187e08cfa5e97
@juanferatl <ferna283@hotmail.com>
2020-06-05 20:59:09 -0500

    Ejerccio Race Car 1

--- src/main/java/com/edu/cedesistemas/oop/model/vehicle/race/Race.java
+++ src/main/java/com/edu/cedesistemas/oop/model/vehicle/race/Race.java
@@ -20,38 +20,54 @@ public class Race<T extends RaceCar> {
     }
 
     public void pits(PitStop<T> pitStop) {
-        T car = pitStop.getCar();
-        pitStop.tank(10);
+        // tanquear el carro
+        pitStop.tank(30);
+        // ajustar el carro
         pitStop.adjust();
+        // cambiar llantas
         pitStop.changeTires();
+        // reparar el carro
         pitStop.repair();
 
         // Adding additional time to car ...
+        T car = pitStop.getCar();
         double extraTime = new Random().nextDouble() * 0.5;
         double time = car.getMovements().get(0).getTime();
         car.getMovements().get(0).setTime(time + extraTime);
 
+        // Reasignar el tiempo del movimiento del carro
         System.out.println("car " + car.getName() + " finishing pits. Extra time: " + extraTime);
     }
 
     public void race() {
         System.out.println("running race with: " + cars.size() + " cars");
-        for (T car : cars) {
-            Segment s = new Segment(Point.of(0, 0), Point.of(100, 100));
-            double time = s.getValue() / car.getSpeed();
-            Vehicle.Movement movement = new Vehicle.Movement(s, time);
-            car.getMovements().add(movement);
+        /*
+        Para cada carro:
+            crear un segment desde (0, 0) hasta (100, 100)
+            calcular el tiempo del segmento: t = x / v --> Formula de velocidad
+            crear un movimiento
+            agregar el movimiento al carro
+        * */
+
+        int boundIni = 0;
+        int boundEnd = 100;
+        for (int i=0; i < cars.size(); i++){
+            Segment segment = new Segment(Point.of(boundIni,boundIni), Point.of(boundEnd,boundEnd));
+            double timeSegment = segment.getValue() / cars.get(i).getSpeed();
+            Vehicle.Movement movement = new Vehicle.Movement(segment, timeSegment);
+            cars.get(i).getMovements().add(movement);
         }
     }
 
     public T getWinner() {
         T winner = null;
-        double minTime = 10;
-        for (T car : cars) {
-            Vehicle.Movement movement = car.getMovements().get(0);
-            if (movement.getTime() < minTime) {
-                minTime = movement.getTime();
-                winner = car;
+        // Obtener el ganador
+        double timeMin = 60;
+        for (int i=0; i < cars.size(); i++){
+            Vehicle.Movement movement = cars.get(i).getMovements().get(0);
+            if (movement.getTime() < timeMin){
+                timeMin = movement.getTime();
+                winner = cars.get(i);
             }
         }
         return winner;
