198c5de3f127529a2a496bf60dc7ffced26201bc
sacgarci <sacgarci@bancolombia.com.co>
2020-06-10 18:45:48 -0500

    version inicial 2

--- src/main/java/com/edu/cedesistemas/oop/model/geometry/GenericPoint.java
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/GenericPoint.java
@@ -1,12 +1,5 @@
 package com.edu.cedesistemas.oop.model.geometry;
 
-<<<<<<< HEAD
-public class GenericPoint<T extends Number>{
-    private T x;
-    private T y;
-
-    public GenericPoint(T x, T y) {
-=======
 import java.util.Objects;
 import java.util.Random;
 
@@ -15,32 +8,10 @@ public class GenericPoint<X extends Number, Y extends Number> {
     private final Y y;
 
     public GenericPoint(X x, Y y) {
->>>>>>> 06995361a37e700caa48f10539ab9b5c8665b907
         this.x = x;
         this.y = y;
     }
 
-<<<<<<< HEAD
-    public T getX() {
-        return x;
-    }
-
-    public T getY() {
-        return y;
-    }
-
-    public static <R extends Number> double distance(GenericPoint<R> p1, GenericPoint<R> p2) {
-        return Math.sqrt(Math.pow(p1.x.doubleValue() - p2.x.doubleValue(), 2)
-                + Math.pow(p1.y.doubleValue() - p2.y.doubleValue(), 2));
-    }
-
-    public static <Y extends Number> GenericPoint<Y> of(Y x, Y y) {
-        GenericPoint<Y> point = new GenericPoint<Y>(x,y);
-        return point;
-    }
-
-}
-=======
     public X getX() {
         return x;
     }
@@ -84,4 +55,4 @@ public class GenericPoint<X extends Number, Y extends Number> {
         return "(" + x + "," + y + ")";
     }
 }
->>>>>>> 06995361a37e700caa48f10539ab9b5c8665b907
+
