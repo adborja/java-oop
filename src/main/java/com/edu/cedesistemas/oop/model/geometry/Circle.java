50464eb548e9bce380cdca14a80a6106f96cda2e
sacgarci <sacgarci@bancolombia.com.co>
2020-06-12 22:52:56 -0500

    version inicial 2

--- src/main/java/com/edu/cedesistemas/oop/model/geometry/Circle.java
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/Circle.java
@@ -1,26 +1,33 @@
 package com.edu.cedesistemas.oop.model.geometry;
 
-public class Circle extends Ellipse implements Scalable<Circle>, ComparableShape {
+public class Circle extends Ellipse implements Scalable<Circle,Number>, ComparableShape {
+    private double radius;
+
     public Circle(double radius) {
         super(radius, radius);
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Circle scale(Number percentage) {
+        return new Circle(getRadius() + getRadius() * percentage.doubleValue() / 100);
+    }
+
     @Override
     public double perimeter() {
-        return 2 * Math.PI * a;
-    }
-
-    public double getRadius() {
-        return a;
-    }
-
-    @Override
-    public Circle scale(double percentage) {
-        return new Circle(getRadius() + getRadius() * percentage / 100);
+        return 2 * Math.PI * radius;
     }
 
     @Override
     public String toString() {
         return "circle {radius: " + getRadius() + "}";
     }
+
 }
