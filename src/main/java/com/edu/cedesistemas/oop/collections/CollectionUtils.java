ede85509aa8a3986d30e12bc8bc1acd0f4ab4ead
@juanferatl <ferna283@hotmail.com>
2020-06-12 23:15:30 -0500

    Taller 3, puntos 2.

--- src/main/java/com/edu/cedesistemas/oop/collections/CollectionUtils.java
+++ src/main/java/com/edu/cedesistemas/oop/collections/CollectionUtils.java
@@ -1,11 +1,13 @@
 package com.edu.cedesistemas.oop.collections;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 public class CollectionUtils {
     public static <E> Set<E> removeDuplicates(Collection<E> s) {
-        return null;
+        Set<E> uniques = new HashSet<>(s);
+        return uniques;
     }
 
     public static <E> Set<E> union(Collection<E> s1, Collection<E> s2) {
