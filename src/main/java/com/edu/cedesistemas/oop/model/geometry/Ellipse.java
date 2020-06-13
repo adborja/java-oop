42e853ae6505ff40527615a32a2058cc84fc2226
Carlos Rojas <genered@gmail.com>
2020-06-12 19:17:58 -0500

    Taller Lección 4 – Funciones-Resolviendo problemas con el merge.

--- src/main/java/com/edu/cedesistemas/oop/model/geometry/Ellipse.java
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/Ellipse.java
@@ -2,15 +2,15 @@ package com.edu.cedesistemas.oop.model.geometry;
 
 public class Ellipse implements Shape {
 
-    private double a;
-    private double b;
+    protected final double a;
+    private final double b;
 
     public Ellipse(double a, double b) {
         this.a = a;
         this.b = b;
     }
 
-    public double getA(){
+    public double getA() {
         return a;
     }
 
@@ -29,3 +29,5 @@ public class Ellipse implements Shape {
         return (2 * Math.PI * Math.sqrt((Math.pow(a, 2) + Math.pow(b, 2)) / 2));
     }
 }
+
+
