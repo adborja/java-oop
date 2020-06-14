af78cef3d52fcfbf257877348cbec7cf3647f536
@juanferatl <ferna283@hotmail.com>
2020-06-13 23:46:00 -0500

    Taller 3, parte b de la clase Workshop.

--- src/main/java/com/edu/cedesistemas/oop/model/vehicles/Workshop.java
+++ src/main/java/com/edu/cedesistemas/oop/model/vehicles/Workshop.java
@@ -4,35 +4,39 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Workshop{
+public class Workshop <C extends Car>{
     private final String name;
-    List<Car> listaCarrosReparacion = new ArrayList<>();
-    List<Car> listaCarrosEntregados = new ArrayList<>();
+    List<C> listaCarrosReparacion = new ArrayList<>();
+    List<C> listaCarrosEntregados = new ArrayList<>();
 
-    public Workshop(String name){
+    public Workshop(String name, List<C> r, List<C> e){
         this.name = name;
+        this.listaCarrosReparacion = r;
+        this.listaCarrosEntregados = e;
     }
 
-    public void repair(Car car){
-        listaCarrosReparacion.add(car);
-        System.out.println("Carro " + car.getName() + " Agregado a lista de carros en Reparación");
+    public void repair(C car){
+        this.listaCarrosReparacion.add(car);
+        System.out.println("Carro: " + car.getName() + " >>> Agregado a lista de carros en Reparación");
     }
 
-    public void deliver(Car car){
-        listaCarrosReparacion.remove(car);
-        System.out.println("Carro " + car.getName() + " Retirado de la lista de carros en Reparación");
+    public void deliver(C car){
+        this.listaCarrosReparacion.remove(car);
+        System.out.println("Carro: " + car.getName() + " >>> Retirado de la lista de carros en Reparación");
         car.setLastMaintenanceDate(LocalDateTime.now());
-        listaCarrosEntregados.add(car);
-        System.out.println("Carro " + car.getName() + " Agregado a lista de carros Entregados");
+        this.listaCarrosEntregados.add(car);
+        System.out.println("Carro: " + car.getName() + " >>> Agregado a lista de carros Entregados");
     }
 
     public void Testworkshop(){
-        Car carElectrico = new ElectricCar(50, "Mazda", "Car Electric");
+        C carElectrico = (C) new ElectricCar(50, "Mazda", "Car Electric");
         repair(carElectrico);
-        Car carDiesel = new DieselCar(30, "Ford", "Diesel Car");
+        C carDiesel = (C) new DieselCar(30, "Ford", "Diesel Car");
         repair(carDiesel);
-        Car carFuel = new FuelCar(30, "Toyota","Car Fuel");
+        C carFuel = (C) new FuelCar(30, "Toyota","Car Fuel");
         repair(carFuel);
+        System.out.println();
+        System.out.println("------------> Nombre Taller: " + this.name + " <------------");
         System.out.println();
         System.out.println(">>> Nro de Carros en REPARACIÓN: " + listaCarrosReparacion.size());
         System.out.println(">>> Nro de Carros ENTREGADOS: " + listaCarrosEntregados.size());
@@ -45,7 +49,15 @@ public class Workshop{
     }
 
     public static void main(String[] args) {
-        Workshop workshop = new Workshop("Taller 1");
+        List<Car> r = new ArrayList<>();
+        List<Car> e = new ArrayList<>();
+        Workshop workshop = new Workshop("El Buen Mecanico", r, e);
         workshop.Testworkshop();
+
+        List<Car> r2 = new ArrayList<>();
+        List<Car> e2 = new ArrayList<>();
+        Workshop workshop2 = new Workshop("Todo Carros", r2, e2);
+        workshop2.Testworkshop();
+
     }
 }
