362d7d74e76413dced2ba44ba6dae5d4c7f9ab4b
adborja <adborja@gmail.com>
2020-06-16 18:51:40 -0500

    Adding functional interfaces

---
+++ src/main/java/com/edu/cedesistemas/oop/model/Employee.java
@@ -0,0 +1,34 @@
+package com.edu.cedesistemas.oop.model;
+
+import java.util.Objects;
+
+public class Employee {
+    private final String id;
+    private final String name;
+
+    public Employee(final String id, final String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id.equals(employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
