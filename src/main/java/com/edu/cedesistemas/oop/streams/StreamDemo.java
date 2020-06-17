cb89e94074c3e1431e6ea8d757261f145ff9c4cf
Jhonatan Plata <jplata@bancolombia.com.co>
2020-06-17 18:43:38 -0500

    Solucion del ejercicio

--- src/main/java/com/edu/cedesistemas/oop/streams/StreamDemo.java
+++ src/main/java/com/edu/cedesistemas/oop/streams/StreamDemo.java
@@ -4,6 +4,7 @@ import com.edu.cedesistemas.oop.model.geometry.Scalable;
 import com.edu.cedesistemas.oop.model.geometry.Shape;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamDemo {
     /**
@@ -14,7 +15,7 @@ public class StreamDemo {
      * @return Lista filtrada con las figuras que tienen un área menor o igual a la especificada.
      * */
     public static List<? extends Shape> filterShapes(double areaLimit, List<? extends Shape> shapes) {
-        return null;
+        return shapes.stream().filter(s -> (s.area()<=areaLimit)).collect(Collectors.toList());
     }
 
     /**
@@ -25,6 +26,6 @@ public class StreamDemo {
      * @return Lista con objetos escalados
      * */
     public static <T extends Scalable<T, U>, U extends Number> List<T> scale(List<T> scalables, U value) {
-        return null;
+        return scalables.stream().map( s -> (s.scale(value))).collect(Collectors.toList());
     }
 }
