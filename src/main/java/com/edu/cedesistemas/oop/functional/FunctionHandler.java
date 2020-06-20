0953cd0a0e397cdee0492ee08458f263300fc651
CarlosRojas <genered@gmail.com>
2020-06-19 19:48:21 -0500

    Lección 4, Teller Reto functions, punto 2.

--- src/main/java/com/edu/cedesistemas/oop/functional/FunctionHandler.java
+++ src/main/java/com/edu/cedesistemas/oop/functional/FunctionHandler.java
@@ -1,10 +1,12 @@
 package com.edu.cedesistemas.oop.functional;
 
-import com.edu.cedesistemas.oop.model.geometry.Point;
-import com.edu.cedesistemas.oop.model.geometry.Rectangle;
+//Lección 4, Teller Reto functions, punto 2.
+
 import com.edu.cedesistemas.oop.model.geometry.Scalable;
 import com.edu.cedesistemas.oop.model.vehicle.Car;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -19,11 +21,26 @@ import java.util.function.Function;
         return f.multiply(shape, value);
     }
 
-    public static  <T> void consume(Consumer<List<T>> consumer, List<T> list) {
-        consumer.accept(list);
-    }
+        public static <T> void consume(Consumer<List<T>> consumer, List<T> list) {
+            consumer.accept(list);
+        }
 
-    public static Function<List<Car>, Map<String, List<Car>>> getCarMapper() {
-        return null;
+        public static Function<List<Car>, Map<String, List<Car>>> getCarMapper() {
+            return c -> MapCars(c);
+        }
+
+        private static Map<String, List<Car>> MapCars(List<Car> c) {
+            Map<String, List<Car>> cars = new HashMap<>();
+            for (Car carList : c) {
+                List<Car> listCar = new ArrayList<Car>();
+                for (Car carList2 : c) {
+                    if (carList2.getName().equals(carList.getName())) {
+                        listCar.add(carList2);
+                    }
+                    cars.put(carList.getName(), listCar);
+                }
+            }
+            return cars;
+        }
+
     }
-}
