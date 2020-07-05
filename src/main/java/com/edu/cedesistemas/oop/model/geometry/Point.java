7d6f759c08283a4e1c72d739f4714e132c9d8a3b
scardona <matsanti9025@gmail.com>
2020-07-04 20:56:09 -0500

    taller 3 Clases genericas

--- src/main/java/com/edu/cedesistemas/oop/model/geometry/Point.java
+++ src/main/java/com/edu/cedesistemas/oop/model/geometry/Point.java
@@ -1,33 +1,34 @@
 package com.edu.cedesistemas.oop.model.geometry;
 
-public class Point {
-    private double x;
-    private double y;
+public class Point<T extends Number> {
+    private T x;
+    private T y;
 
-    public Point(double x,double y) {
+    public Point(T x,T y) {
         this.x = x;
         this.y = y;
     }
 
-    public double getX() {
-        return x;
+    public <T extends Number> double getX() {
+        return x.doubleValue();
     }
 
-    public double getY() {
-        return y;
+    public <T extends Number> double getY() {
+        return y.doubleValue();
     }
 
 
-    public static double distance(Point p1, Point p2) {
-        return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+    public static <T extends Number> double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x.doubleValue() - p2.x.doubleValue(), 2)
+                + Math.pow(p1.y.doubleValue() - p2.y.doubleValue(), 2));
     }
 
-    public static Point of(double x, double y) {
+    public static <T extends Number> Point of(T x, T y) {
         Point point = new Point(x,y);
         return point;
     }
 
-    public static Point random(int x) {
+    public static <T extends Number> Point random(T x) {
         Point point = new Point(x,x);
         return point;
     }
